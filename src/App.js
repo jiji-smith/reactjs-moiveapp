dbcfb52fa1143aab944d16b76cc88032517a840e
Ji Smith <jihyunjung0219@gmail.com>
2021-03-21 22:26:36 -0500

    ready to be css-ed

--- src/App.js
+++ src/App.js
@@ -27,20 +27,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-    <div>
-      {isLoading
-       ? "Loading.."
-       : movies.map(movie => (
-          <Movie
-            key={movie.id}
-            id={movie.id}
-            year={movie.year}
-            title={movie.title}
-            summary={movie.summary}
-            poster={movie.medium_cover_image}
-          />
-       ))}
-    </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                poster={movie.medium_cover_image}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
